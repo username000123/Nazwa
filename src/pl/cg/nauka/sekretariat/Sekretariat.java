d7c3fa4139cc44a7ccb8129eec54efc5b666890a
Mario Kebab <wewwewoes@gmail.com>
2023-03-08 08:47:19 +0100

    innyopis

--- src/pl/cg/nauka/sekretariat/Sekretariat.java
+++ src/pl/cg/nauka/sekretariat/Sekretariat.java
@@ -3,5 +3,7 @@ package pl.cg.nauka.sekretariat;
 public class Sekretariat {
     public static void main(String[] args) {
         Kartoteka kartoteka=new KartotekaLista();
+
+        System.out.println("test");
     }
 }
